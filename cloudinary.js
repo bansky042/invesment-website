175544b6d1295feb1a26f9db0240e58f6aea0e7e
bansky <abanakosisochukwu03@gmail.com>
2025-04-18 00:06:04 +0100

    ok

--- cloudinary.js
+++ cloudinary.js
@@ -3,6 +3,13 @@ const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+
+console.log("Cloudinary ENV:", {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  
 // 🔐 Config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
